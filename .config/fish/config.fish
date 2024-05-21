8a5b9ab71421a206bab0a3f20f9e32942c2d65c2
Rignchen <rinchendemort@gmail.com>
2024-05-21 08:35:23 +0200

    add zoxide

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,4 +7,6 @@ alias "cls=clear && fastfetch"
 alias "Tconfig=$EDITOR ~/.config/fish/config.fish"
 alias "Thistory=$EDITOR ~/.config/fish/fish_history"
 
+# execution
+zoxide init fish | source
 cls
