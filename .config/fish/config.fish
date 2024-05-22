ba2815582aabfc7a532cd7c8617d955f90d64d83
Rignchen <rinchendemort@gmail.com>
2024-05-22 23:10:33 +0200

    edit fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
 
 # export
 export "EDITOR=vim"
@@ -5,7 +8,8 @@ export "EDITOR=vim"
 # alias
 alias "cls=clear && fastfetch"
 alias "Tconfig=$EDITOR ~/.config/fish/config.fish"
-alias "Thistory=$EDITOR ~/.config/fish/fish_history"
+alias "Thistory=$EDITOR ~/.local/share/fish/fish_history"
+alias "Vimrc=$EDITOR ~/.vimrc"
 
 # execution
 zoxide init fish | source
