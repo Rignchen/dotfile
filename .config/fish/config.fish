0866e0eb64ae439c14aae8f5d22adc3dcc987b06
Rignchen <rinchendemort@gmail.com>
2024-05-24 19:22:25 +0200

    su -> sudo su, + copy to clipboard

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,8 @@ alias "cls=clear && fastfetch"
 alias "Tconfig=$EDITOR ~/.config/fish/config.fish"
 alias "Thistory=$EDITOR ~/.local/share/fish/fish_history"
 alias "Vimrc=$EDITOR ~/.vimrc"
+alias "su=sudo su"
+alias "copy=xclip -selection clipboard"
 
 # execution
 zoxide init fish | source
