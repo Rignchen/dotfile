ff8bbfe09a45e5c950c9273b234026f91157f4e3
Rignchen <rinchendemort@gmail.com>
2024-09-11 15:21:45 +0200

    fix(cmd): correct keybinds from wsl

--- .config/zsh/fix.zsh
+++ .config/zsh/fix.zsh
@@ -1,3 +1,20 @@
 # Fix some bugs in zsh
+
+## Simple
+# delete
+bindkey "^[[3~" delete-char
+# home
+bindkey "^[[1~" beginning-of-line
+# end
+bindkey "^[[4~" end-of-line
+
+## Ctrl
+# ctrl+left
 bindkey "^[[1;5C" forward-word
+# ctrl+right
 bindkey "^[[1;5D" backward-word
+# ctrl+backspace
+bindkey "^H" backward-kill-word
+# ctrl+delete
+bindkey "^[[3;5~" kill-word
+
