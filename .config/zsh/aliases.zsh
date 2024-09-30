3aca3ed2519521823808d5017cdf48f89f498747
Rignchen <rinchendemort@gmail.com>
2024-09-30 13:44:10 +0200

    feat(cmd): new docker ps command

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -8,7 +8,6 @@ alias "cd....=cd ../../../.."
 alias "ls=ls --color=auto"
 alias "la=ls -laF"
 alias "ll=ls -lF"
-command -v pnpm && alias "npm=pnpm"
 
 # os specific
 case $(cat /etc/os-release | grep "^ID=" | sed -e "s/^ID=//") in
@@ -35,4 +34,6 @@ alias "mkdir=mkdir -p"
 alias "fetch=fastfetch || neofetch"
 alias "cls=clear && fetch"
 alias ":q=exit"
+command -v pnpm && alias "npm=pnpm"
+alias "docker_ps=docker ps --format 'table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}'"
 
