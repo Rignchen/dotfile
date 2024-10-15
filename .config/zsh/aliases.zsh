18a735f018ac9b073d66f8feda422b1e951a214f
Rignchen <rinchendemort@gmail.com>
2024-10-15 17:09:26 +0200

    fix(cmd): pg_dump have right stdout

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -37,5 +37,5 @@ alias ":q=exit"
 command -v pnpm && alias "npm=pnpm"
 alias "docker_ps=docker ps --format 'table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}'"
 alias "psql=docker run --name some-postgres --volume /var/lib/postgresql/data:/var/lib/postgresql/data -e POSTGRES_HOST_AUTH_METHOD=trust -d postgres && sleep 5 && docker exec -it some-postgres psql -U postgres; docker rm -f some-postgres"
-alias "pg_dump=docker run --name some-postgres --volume /var/lib/postgresql/data:/var/lib/postgresql/data -e POSTGRES_HOST_AUTH_METHOD=trust -d postgres > /dev/null && sleep 5 && docker exec -it some-postgres pg_dump -U postgres; docker rm -f some-postgres"
+alias "pg_dump=docker run --name some-postgres --volume /var/lib/postgresql/data:/var/lib/postgresql/data -e POSTGRES_HOST_AUTH_METHOD=trust -d postgres > /dev/null && sleep 5 && TEMP=\$(docker exec -it some-postgres pg_dump -U postgres); docker rm -f some-postgres > /dev/null && echo \$TEMP"
 
