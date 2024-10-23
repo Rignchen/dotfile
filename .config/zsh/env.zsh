c77dc7e38adfb744bdffae803cfcd1822e61d9bc
Rignchen <rinchendemort@gmail.com>
2024-10-23 19:01:50 +0200

    feat(zsh): move computer specific config in it's own file

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -4,5 +4,5 @@ export OMP_CONFIG="theme.toml"
 
 # env
 export OS="$(cat /etc/os-release | grep "^ID=" | cut --delimiter "=" --fields 2)"
-export PATH="$PATH:$HOME/.local/bin:$HOME/.local/share/JetBrains/Toolbox/scripts:$HOME/cmd:$HOME/Discord"
+export PATH="$PATH:$HOME/.local/bin:$HOME/.local/share/JetBrains/Toolbox/scripts:$HOME/cmd"
 export EDITOR='vim'
