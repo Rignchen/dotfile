a78457cf2666ebeceeb2107e95b271af5f75a054
Rignchen <rinchendemort@gmail.com>
2024-11-07 10:44:06 +0100

    feat(cmd): change default editor to neovim

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -5,4 +5,4 @@ export OMP_CONFIG="theme.toml"
 # env
 export OS="$(cat /etc/os-release | grep "^ID=" | cut --delimiter "=" --fields 2)"
 export PATH="$PATH:$HOME/.local/bin:$HOME/.local/share/JetBrains/Toolbox/scripts:$HOME/cmd"
-export EDITOR='vim'
+export EDITOR='nvim'
