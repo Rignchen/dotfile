a715f045cd44a772e3b4480face87f1973110e67
Rignchen <rinchendemort@gmail.com>
2024-11-07 23:28:46 +0100

    refactor(nvim): use nvim version of lf instead of vim one

--- .config/nvim/lua/plugins/fileExplorer.lua
+++ .config/nvim/lua/plugins/fileExplorer.lua
@@ -1,14 +1,20 @@
 return {
 	{
-		"ptzz/lf.vim",
-		lazy = false, --ensure that the plugin is loaded
+		"lmburns/lf.nvim",
+		lazy = false, --the plugin needs to be loaded to be able to call :Lf
 		config = function()
+			require("lf").setup({
+				escape_quit = true,
+				border = "curved",
+				default_file_manager = true, -- use lf when opening in a directory
+			})
+
 			vim.keymap.set('n', '-', ':Lf<CR>')
 		end,
 	},
 	{
-		"voldikss/vim-floaterm",
-		lazy = false, --ensure that the plugin is loaded
+		"toggleterm.nvim",
+		lazy = true, --this plugin can stay unloaded until it's called
 	}
 }
 
