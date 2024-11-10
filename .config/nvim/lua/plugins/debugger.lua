717c14ee529a55c91295fa7bff92e10183d310d9
Rignchen <rinchendemort@gmail.com>
2024-11-10 13:26:00 +0100

    feat(nvim): use a debugger made for neovim instead of vim

--- .config/nvim/lua/plugins/debugger.lua
+++ .config/nvim/lua/plugins/debugger.lua
@@ -1,6 +1,15 @@
 return {
 	{
-		"puremourning/vimspector",
+		"mfussenegger/nvim-dap",
 		lazy = false,
-	}
+		dependencies = {
+			"theHamsta/nvim-dap-virtual-text",
+			"rcarriga/nvim-dap-ui",
+			-- languages
+			"mfussenegger/nvim-dap-python",
+		},
+		config = function()
+			require("dap-python").setup("/run/current-system/sw/bin/python3")
+		end,
+	},
 }
