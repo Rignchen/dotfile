ead0fd40931a1e7d437996bd2816082251db9296
Rignchen <rinchendemort@gmail.com>
2025-01-08 15:50:11 +0100

    feat(nvim): allow to take a part of copilor's suggestion

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -2,5 +2,8 @@ return {
 	{
 		"github/copilot.vim",
 		lazy = false, -- make sure we load this during startup
+		config = function()
+			vim.keymap.set('i', '<C-Right>', '<Plug>(copilot-accept-word)')
+		end
 	}
 }
