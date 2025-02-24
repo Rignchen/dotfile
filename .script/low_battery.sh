19cb7a324657a04843aa5695c49e87b102d46c0b
Rignchen <rinchendemort@gmail.com>
2025-02-24 08:54:33 +0100

    refactor(hypr): use variable for battery-notif parameters

--- .script/low_battery.sh
+++ .script/low_battery.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+WARNING_PERCENTAGE=60
+NOTIFICATION_TIMEOUT=5000
+SLEEP_BETWEEN_CHECKS=120
+
 while true; do
-    battery=$(upower -i "$(upower -e | grep BAT)" | grep -E "percentage" | awk '{print $2}' | tr -d '%')
-    if [ "$battery" -le "60" ]; then
-        hyprctl notify 0 5000 0 "  Low battery: ${battery}%" 
-        sleep 240
-    else
-        sleep 120
-    fi
+	battery=$(upower -i "$(upower -e | grep BAT)" | grep -E "percentage" | awk '{print $2}' | tr -d '%')
+	if [ "$battery" -le "$WARNING_PERCENTAGE" ]; then
+		hyprctl notify 0 $NOTIFICATION_TIMEOUT 0 "  Low battery: ${battery}%"
+		sleep $SLEEP_BETWEEN_CHECKS
+	else
+		echo "Battery is at ${battery}%"
+		sleep $SLEEP_BETWEEN_CHECKS
+	fi
 done
